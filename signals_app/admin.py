abc07ebea189080d71ecded5c2b165eb8bed289f
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-06-09 20:10:07 +0530

    Refactor admin.py to register models with their respective ModelAdmin classes

--- signals_app/admin.py
+++ signals_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import SyncTestModel, LogModel,ThreadTestModel,TransactionTestModel
 
 # Register your models here.
+
+class SyncTestModelAdmin(admin.ModelAdmin):
+    list_display = ('id','name',)
+class ThreadTestModelAdmin(admin.ModelAdmin):
+    list_display = ('id','name',)
+class TransactionTestModelAdmin(admin.ModelAdmin):
+    list_display = ('id','name',)
+class LogModelAdmin(admin.ModelAdmin):
+    list_display = ('id','message',)
+
+
+admin.site.register(SyncTestModel,SyncTestModelAdmin)
+admin.site.register(ThreadTestModel,ThreadTestModelAdmin)
+admin.site.register(TransactionTestModel,TransactionTestModelAdmin)
+admin.site.register(LogModel,LogModelAdmin)
