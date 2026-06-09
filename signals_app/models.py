ea5c1c21876b93410ed6d5ee4048b5e5994b269e
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-06-09 20:06:57 +0530

    Add LogModel, ThreadTestModel, and TransactionTestModel; rename TestModel to SyncTestModel

--- signals_app/models.py
+++ signals_app/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
-class TestModel(models.Model):
+class SyncTestModel(models.Model):
     name = models.CharField(max_length=100)
+
+class ThreadTestModel(models.Model):
+    name = models.CharField(max_length=100)
+
+class TransactionTestModel(models.Model):
+    name = models.CharField(max_length=100)
+
+class LogModel(models.Model):
+    message = models.CharField(max_length=100)
