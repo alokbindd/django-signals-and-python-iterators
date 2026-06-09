c5e7b1351ec389b8cd0810c7849d3a50cbd2515b
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-06-09 20:09:40 +0530

    Add views for signal testing: sync, thread, and transaction

--- signals_app/views.py
+++ signals_app/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+import time
+import threading
+from django.http import HttpResponse
+from django.db import transaction
+from .models import SyncTestModel, LogModel,ThreadTestModel,TransactionTestModel
 
-# Create your views here.
+def test_sync_signal(request):
+    start_time = time.time()
+
+    SyncTestModel.objects.create(name="Test")
+
+    end_time = time.time()
+
+    total_time = end_time - start_time
+
+    return HttpResponse(
+        f"Time taken: {total_time:.2f} seconds"
+    )
+
+signal_thread_id = None
+
+def test_thread_signal(request):
+    global signal_thread_id
+
+    caller_thread_id = threading.get_ident()
+
+    ThreadTestModel.objects.create(name="Thread Test")
+
+    return HttpResponse(
+        f"Caller Thread: {caller_thread_id}<br>"
+        f"Signal Thread: {signal_thread_id}"
+    )
+
+def test_transaction_signal(request):
+    TransactionTestModel.objects.all().delete()
+    LogModel.objects.all().delete()
+
+    try:
+        with transaction.atomic():
+            TransactionTestModel.objects.create(
+                name="Transaction Test"
+            )
+
+            raise Exception("Force RollBack")
+    except Exception:
+        pass
+
+    transaction_test_count = TransactionTestModel.objects.count()
+    log_count = LogModel.objects.count()
+
+    return HttpResponse(
+        f"Transaction Model Count = {transaction_test_count}<br>"
+        f"LogModel Count = {log_count}"
+    )
